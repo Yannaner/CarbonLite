85fe7bb9c894a47f34c94120b996bc54b3ee600c
Ian <2005fong@gmail.com>
2025-02-05 14:50:12 -0500

    BAckend update

--- matlab-env/test.m
+++
@@ -1,8 +0,0 @@
-% Test if the Wav2Vec model is available
-try
-    client = speechClient("wav2vec2.0");
-    disp('Wav2Vec model loaded successfully!');
-catch ME
-    disp('Error loading model:');
-    disp(ME.message);
-end
